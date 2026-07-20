12e9dac14c2bc8778aa5d309177bd232a5f740e3
usario172332 <usario172332@gmail.com>
2026-07-21 00:25:23 +0200

    Update AppShell.jsx

--- youraifood-app/components/AppShell.jsx
+++ youraifood-app/components/AppShell.jsx
@@ -2,13 +2,17 @@
 
 import { AuthProvider } from '../lib/AuthContext';
 import Sidebar from './Sidebar';
+import Footer from './Footer';
 
 export default function AppShell({ children }) {
   return (
     <AuthProvider>
       <div className="md:flex">
         <Sidebar />
-        <main className="min-w-0 flex-1">{children}</main>
+        <main className="min-w-0 flex-1">
+          {children}
+          <Footer />
+        </main>
       </div>
     </AuthProvider>
   );
